88d1e81ee7869ab2007f49bf30288e9ba7583e4f
unknown <ak248100@gmail.com>
2020-02-07 12:27:58 +0530

    ''

--- grad.py
+++ grad.py
@@ -12,12 +12,12 @@ X= (X-np.min(X))/(np.max(X)-np.min(X))
 # print(X)
 # print(X.T)
 Y = dataset["Brain Weight(grams)"].values
-
+Y= (Y-np.min(Y))/(np.max(Y)-np.min(Y))
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=1)
 m = 0
 c = 0
 
-L = 0.0001  # The learning Rate
+L = 0.1  # The learning Rate
 epochs = 1000  # The number of iterations to perform gradient descent
 
 n = float(len(X)) # Number of elements in X
@@ -28,6 +28,12 @@ for i in range(epochs):
     D_m = (-2/n) * sum(X * (Y - Y_pred))  # Derivative wrt m
     D_c = (-2/n) * sum(Y - Y_pred)  # Derivative wrt c
     m = m - L * D_m  # Update m
-    c = c - L * D_c  # Update c
+    c = c - L * D_c  # Update c 
     
 print (m, c)
+# Making predictions
+Y_pred = m*X + c
+
+plt.scatter(X, Y) 
+plt.plot([min(X), max(X)], [min(Y_pred), max(Y_pred)], color='red')  # regression line
+plt.show()
